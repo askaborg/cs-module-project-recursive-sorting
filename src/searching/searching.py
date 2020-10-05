cd644df31269f57f5cff8fc9c28fa8811ad1b642
askaborg <hienle@askaborg.com>
2020-10-05 14:27:12 -0700

    sort and search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,20 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if len(arr) == 0: # empty array index not found
+        return -1
+    middle_index = (start + end) // 2 # midway point
+    middle_value = arr[middle_index] # value of the midway
 
+    if middle_value == target: # value found 
+        return middle_index # return the index of the found value
+    else:
+        if middle_value < target: # if the midpoint value is less than the target value
+            return binary_search(arr, target, middle_index, end) # run binary search starting from the midway point to the end
+        if middle_value > target: # if the midpoint value is greater than the target value
+            return binary_search(arr, target, start, middle_index) # run binary search starting from the start to the midway point
+    
+    return -1 # index not found
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +24,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,14 +4,56 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
-
+    indexA = 0 # index for arrayA
+    indexB = 0 # index for arrayB
+    mergeIndex = 0 # index of merged arr
+          
+    # when indexA is less then length of arrayA and
+    # indexB is less than arrayB (both arrays contain values)
+    while indexA < len(arrA) and indexB < len(arrB):
+        # if the value of arrA at the indexA is greater than value of arrB at the indexB
+        # set the index of the mergeIndex to the index of A
+        # increment the indexA
+        if arrA[indexA] < arrB[indexB]: 
+            merged_arr[mergeIndex] = arrA[indexA] 
+            indexA += 1 
+        # the value of arrB at the indexB is greater than arrA at the indexA
+        # set the index of the mergeIndex to the index of B
+        # increment the indexB
+        # increment the mergeIndex
+        else: 
+            merged_arr[mergeIndex] = arrB[indexB] 
+            indexB += 1 
+        mergeIndex += 1 
+    # when arrayA is not empty but arrayB is
+    while indexA < len(arrA): 
+        # set the index of the merged_arr to the arrA index
+        # increment the indexes
+        merged_arr[mergeIndex] = arrA[indexA]
+        indexA += 1
+        mergeIndex += 1
+    # when arrayB not empty
+    while indexB < len(arrB): 
+        # set the index of the merged_arr to the arrB index
+        # increment the indexes
+        merged_arr[mergeIndex] = arrB[indexB] 
+        indexB += 1
+        mergeIndex += 1
+    
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
-
+    # when arr is not empty
+    # set the mid variable to be half of the arr
+    # set left to be everything up to the middle
+    # set right to be everything after the middle
+    if len(arr) > 1:
+        middle = len(arr) // 2
+        left = arr[:middle]
+        right = arr[middle:]
+        return merge(merge_sort(left), merge_sort(right))
 
     return arr
 
@@ -21,8 +63,8 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
